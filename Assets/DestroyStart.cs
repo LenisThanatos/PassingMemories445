449792ac00021ac77bec05209ec341e963e219d0
LenisThanatos <61440485+LenisThanatos@users.noreply.github.com>
2020-04-13 18:10:20 -0700

    Cabin Changes

---
+++ Assets/DestroyStart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter (Collision coll)
+    {
+        if (coll.gameObject.tag == "Start")
+        {
+            GameObjects[] gameObjects = GameObject.FindGameObjectsWithTag(Start);
+            foreach (GameObject target in gameObject) 
+            {
+                gameObject.Destroy(target);
+                Destroy(coll.gameObject);
+            }
+        }
+    }
+}
